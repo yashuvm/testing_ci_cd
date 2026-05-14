ad3697d37327460fae170a931179e2d4277ad691
yashuvm <48407518+yashuvm@users.noreply.github.com>
2026-05-14 13:13:58 +0000

    feat:fearure8005 new file upload

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ message: "Everything is good"})
+    res.status(200).json({ message: "Everything is goods"})
 });
 
 
